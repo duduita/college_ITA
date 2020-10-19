e09baa7b5f98e9c89d82b3d4452619a31054530a
Eduardo Menezes Moraes <eduardomenezesm@msn.com>
2020-10-19 00:15:37 -0300

    worked with users in login system

--- chatsystem/chatJS/app.js
+++ chatsystem/chatJS/app.js
@@ -1,7 +1,7 @@
-var app = require('http').createServer(resposta);
-var fs = require('fs');
-var io = require('socket.io')(app);
-
+var app = require("http").createServer(resposta);
+var fs = require("fs");
+var io = require("socket.io")(app);
+var usuarios = [];
 
 app.listen(3000);
 console.log("Aplicação está em execução...");
@@ -9,41 +9,64 @@ console.log("Aplicação está em execução...");
 function resposta(req, res) {
     var arquivo = "";
     if (req.url == "/") {
-        arquivo = __dirname + '/index.html';
+        arquivo = __dirname + "/index.html";
     } else {
         arquivo = __dirname + req.url;
     }
-    fs.readFile(arquivo,
-        function(err, data) {
-            if (err) {
-                res.writeHead(404);
-                return res.end('Página ou arquivo não encontrados');
-            }
-
-            res.writeHead(200);
-            res.end(data);
+    fs.readFile(arquivo, function(err, data) {
+        if (err) {
+            res.writeHead(404);
+            return res.end("Página ou arquivo não encontrados");
         }
-    );
+
+        res.writeHead(200);
+        res.end(data);
+    });
+}
+
+function pegarDataAtual() {
+    var dataAtual = new Date();
+    var dia = (dataAtual.getDate() < 10 ? "0" : "") + dataAtual.getDate();
+    var mes =
+        (dataAtual.getMonth() + 1 < 10 ? "0" : "") + (dataAtual.getMonth() + 1);
+    var ano = dataAtual.getFullYear();
+    var hora = (dataAtual.getHours() < 10 ? "0" : "") + dataAtual.getHours();
+    var minuto =
+        (dataAtual.getMinutes() < 10 ? "0" : "") + dataAtual.getMinutes();
+    var segundo =
+        (dataAtual.getSeconds() < 10 ? "0" : "") + dataAtual.getSeconds();
+
+    var dataFormatada =
+        dia + "/" + mes + "/" + ano + " " + hora + ":" + minuto + ":" + segundo;
+    return dataFormatada;
 }
 
 io.on("connection", function(socket) {
-    socket.on("enviar mensagem", function(mensagem_enviada, callback) {
-        mensagem_enviada = "[ " + pegarDataAtual() + " ]: " + mensagem_enviada;
+    socket.on("entrar", function(apelido, callback) {
+        if (!(apelido in usuarios)) {
+            socket.apelido = apelido;
+            usuarios[apelido] = socket;
 
+            io.sockets.emit("atualizar usuarios", Object.keys(usuarios));
+            io.sockets.emit(
+                "atualizar mensagens",
+                "[ " + pegarDataAtual() + " ] " + apelido + " acabou de entrar na sala"
+            );
+
+            callback(true);
+        } else {
+            callback(false);
+        }
+    });
+    socket.on("enviar mensagem", function(mensagem_enviada, callback) {
+        mensagem_enviada =
+            "[ " +
+            pegarDataAtual() +
+            " ] " +
+            socket.apelido +
+            " diz: " +
+            mensagem_enviada;
         io.sockets.emit("atualizar mensagens", mensagem_enviada);
         callback();
     });
 });
-
-function pegarDataAtual() {
-    var dataAtual = new Date();
-    var dia = (dataAtual.getDate() < 10 ? '0' : '') + dataAtual.getDate();
-    var mes = ((dataAtual.getMonth() + 1) < 10 ? '0' : '') + (dataAtual.getMonth() + 1);
-    var ano = dataAtual.getFullYear();
-    var hora = (dataAtual.getHours() < 10 ? '0' : '') + dataAtual.getHours();
-    var minuto = (dataAtual.getMinutes() < 10 ? '0' : '') + dataAtual.getMinutes();
-    var segundo = (dataAtual.getSeconds() < 10 ? '0' : '') + dataAtual.getSeconds();
-
-    var dataFormatada = dia + "/" + mes + "/" + ano + " " + hora + ":" + minuto + ":" + segundo;
-    return dataFormatada;
-}
--- chatsystem/chatJS/script.js
+++ chatsystem/chatJS/script.js
@@ -3,12 +3,39 @@ $(document).ready(function() {
 
     $("form#chat").submit(function(e) {
         e.preventDefault();
-        socket.emit("enviar mensagem", $(this).find("#texto_mensagem").val(), function() {
-            $("form#chat #texto_mensagem").val("");
+        socket.emit(
+            "enviar mensagem",
+            $(this).find("#texto_mensagem").val(),
+            function() {
+                $("form#chat #texto_mensagem").val("");
+            }
+        );
+    });
+    socket.on("atualizar mensagens", function(mensagem) {
+        var mensagem_formatada = $("<p />").text(mensagem);
+        $("#historico_mensagens").append(mensagem_formatada);
+    });
+
+    $("form#login").submit(function(e) {
+        e.preventDefault();
+
+        socket.emit("entrar", $(this).find("#apelido").val(), function(valido) {
+            if (valido) {
+                $("#acesso_usuario").hide();
+                $("#sala_chat").show();
+            } else {
+                $("#acesso_usuario").val("");
+                alert("Nome já utilizado nesta sala");
+            }
         });
-        socket.on("atualizar mensagens", function(mensagem) {
-            var mensagem_formatada = $("<p />").text(mensagem);
-            $("#historico_mensagens").append(mensagem_formatada);
+    });
+
+    socket.on("atualizar usuarios", function(usuarios) {
+        $("#lista_usuarios").empty();
+        $("#lista_usuarios").append("<option value=''>Todos</option>");
+        $.each(usuarios, function(indice) {
+            var opcao_usuario = $("<option />").text(usuarios[indice]);
+            $("#lista_usuarios").append(opcao_usuario);
         });
     });
 });
