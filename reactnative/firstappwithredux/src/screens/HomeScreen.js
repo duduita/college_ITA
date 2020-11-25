0533c4e7aa566227c30910f2dee7276bbb510077
Eduardo Menezes Moraes <eduardomenezesm@msn.com>
2020-11-25 20:51:44 -0300

    added with redux

---
+++ reactnative/firstappwithredux/src/screens/HomeScreen.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import DeprecatedViewPropTypes from "react-native/Libraries/DeprecatedPropTypes/DeprecatedViewPropTypes";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello!</Text>
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to Components Demo"
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+      ></Button>
+      <Button
+        title="Go to Image Demo"
+        onPress={() => navigation.navigate("Image")}
+      ></Button>
+      <Button
+        title="Go to Counter"
+        onPress={() => navigation.navigate("Counter")}
+      ></Button>
+      <Button
+        title="Go to Color"
+        onPress={() => navigation.navigate("Color")}
+      ></Button>
+      <Button
+        title="Go to Square"
+        onPress={() => navigation.navigate("Square")}
+      ></Button>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default HomeScreen;
